6b1fc5d3dd1bda8049589d602bff40484e832934
DAVID COSTA GOMES <david.cgomes@a.ucb.br>
2025-02-28 10:33:27 -0300

    Termino da primeira aula

---
+++ aula01/variaveis.js
@@ -0,0 +1,10 @@
+let NAAAOOOO = "STRING FODA!!!";
+let silencio = 69;
+console.log(NAAAOOOO);
+
+console.log(`Aki esta ume exemplo bom de formatação: ${NAAAOOOO} , ${silencio}`);
+
+let cinco = 5;
+let sete = 7;
+
+console.log(`5 + 7 = ${cinco + sete}`);
