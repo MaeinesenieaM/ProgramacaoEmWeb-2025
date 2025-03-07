4dcc59537024b06da2826992202ad94955064cc0
MaeinesenieaM <david.costa.gomes71@gmail.com>
2025-03-07 09:13:21 -0300

    variaveisOperadores.js Modularizado.

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,20 +1,29 @@
 //Por David Costa Gomes UC23200191
 
-let operacoes = [];
-operacoes.adicao = "adição";
-operacoes.subtracao = "subtração";
-operacoes.multiplicao = "multiplicação";
-operacoes.divisao = "divisão";
+let operador = [];
+operador.adicao = "adição";
+operador.subtracao = "subtração";
+operador.multiplicao = "multiplicação";
+operador.divisao = "divisão";
+
+function calcular(num_a, num_b, operador) {
+    let resultado;
+    switch (operador) {
+        case "adição": resultado = num_a + num_b; break;
+        case "subtração": resultado = num_a - num_b; break;
+        case "multiplicação": resultado = num_a * num_b; break;
+        case "divisão": resultado = num_a / num_b; break;
+        default: return;
+    }
+    console.log(`Resultado da ${operador} entre ${num_a} e ${num_b} = ${resultado}`);
+}
 
 let num_a = 20;
 let num_b = 5;
-let resultado;
 
-resultado = num_a + num_b;
-console.log(`Resultado da ${operacoes.adicao} entre ${num_a} e ${num_b} = ${resultado}`);
-resultado = num_a - num_b;
-console.log(`Resultado da ${operacoes.subtracao} entre ${num_a} e ${num_b} = ${resultado}`);
-resultado = num_a * num_b;
-console.log(`Resultado da ${operacoes.multiplicao} entre ${num_a} e ${num_b} = ${resultado}`);
-resultado = num_a / num_b;
-console.log(`Resultado da ${operacoes.divisao} entre ${num_a} e ${num_b} = ${resultado}`);
+calcular(num_a, num_b, operador.adicao);
+calcular(num_a, num_b, operador.subtracao);
+calcular(num_a, num_b, operador.multiplicao);
+calcular(num_a, num_b, operador.divisao);
+
+
